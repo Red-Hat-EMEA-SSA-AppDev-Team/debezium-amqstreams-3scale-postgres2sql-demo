165022f5de8f5669c9f0a38c83a3a255180fd9a6
Sergio <sgutierr@redhat.com>
2022-05-05 17:33:42 +0200

    Create create-table-order1.sql

---
+++ create-table-order1.sql
@@ -0,0 +1,10 @@
+CREATE TABLE order1
+(
+   id int NOT NULL,
+   order_date int NULL,
+   purchaser int NULL,
+   quantity int NULL,   
+   product_id int NULL,       
+  CONSTRAINT OrderID
+  PRIMARY KEY (id)
+);
